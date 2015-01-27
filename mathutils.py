c5cd9139b509f095ae7df285931b73e3d48ebeab
John Swoboda <swoboj@bu.edu>
2015-01-27 16:54:17 -0500

    Updated sommerfeld integration routines to allow for multiple iterations.

--- mathutils.py
+++ mathutils.py
@@ -73,15 +73,38 @@ def chirpz(Xn,A,W,M):
 
     return yk
 
-def sommerfeldchirpz(func,N,M,dk,exparams=None,a=-1.0*1j,p=1.0,x0=None):
+def sommerfeldchirpz(func,N,M,dk,Lmax=1,errF=0.1,a=-1.0j,p=1.0,x0=None,exparams=()):
+    """ sommerfeldchirpz(func,N,M,dk,Lmax=1,errF=0.1,a=-1.0*1j,p=1.0,x0=None,exparams=())
+        by John Swoboda
+        This function will numerically integrate a Sommerfeld like integral, int(exp(awk)f(k),t=0..inf)
+        using at most Lmax number of N length chirpz transforms to make an M length
+        array. If the normalized difference between the previous estimate of the output Xk is
+        less then the parameter errF then the loop stops and a flag that represents convergence is set to true.
+        A number of repeats are also output as well. This technique is based off the article
+        Li et. al Adaptive evaluation of the Sommerfeld-type integral using the chirp z-transform, 1991.
+        This function also uses a modified Simpsons rule for integration found in Milla PhD Thesis (2010).
+
+        Inputs
+        func: A function that is used to create f(k).
+        N: The length of the chirp z transform used.
+        M: The length of the output array.
+        dk: The sample period of k.
+        Lmax: default 1:The maximum number of repeats of the integration before the loop finishes.
+        errF: default .1: The threshold of the normalized difference between the new
+        iteration and the old to stop to stop the iteration.
+        a: default -1.0*1j: A complex number that determines the trejectory of the integration on the z plane.
+        p: default 1.0: A real number that helps to define the spacing on the omega plane.
+        x0: default p*np.pi/dk: The starting point on the omega plane.
+        exparams: default (): Any extra params other then k to create f(k).
+        output
+        Xk - The integrated data that is of length M.
+        flag_c - A convergence flag.
+        irep - The number of repitiions until convergence."""
+
 
     k = np.arange(N)*dk
     if x0 is None:
-        x0 = np.pi/dk
-    if exparams is None:
-        fk = func(k)
-    else:
-        fk = func(k,*exparams)
+        x0 = p*np.pi/dk
 
     wk = np.ones(N)
     wk[np.mod(k,2)==0] = 2.0/3.0
@@ -91,11 +114,71 @@ def sommerfeldchirpz(func,N,M,dk,exparams=None,a=-1.0*1j,p=1.0,x0=None):
     A_0 = np.exp(a*dk*x0)
     M2 = M - np.mod(M,2)
     W_0 = np.exp(a*2.0*p*np.pi/M2)
-    Xk = chirpz(fk*wk,A_0,W_0,M)
-    return Xk
+    freqm = np.arange(-np.ceil((M-1)/2.0),np.floor((M-1)/2.0))
+    Xk = np.zeros(M)*1j
+    flag_c = False
+    for irep in range(Lmax):
 
-def sommerfelderf(func,N,theta,a,b,exparams=None):
+        fk = func(k+N*dk*irep,*exparams)
+        Xkold = Xk
+        Xk = chirpz(fk*wk,A_0,W_0,M)*np.power(W_0,N*dk*irep*freqm)+Xk
 
+        Xkdiff = np.sqrt(np.sum(np.power(np.abs(Xk-Xkold),2.0)))
+        Xkpow = np.sqrt(np.sum(np.power(np.abs(Xk))))
+        if Xkdiff/Xkpow<errF:
+            flag_c = True
+            break
+
+    return (Xk,flag_c,irep+1)
+
+
+def sommerfelderfrep(func,N,omega,b1,Lmax=1,errF=0.1,exparams=()):
+    """ sommerfelderfrep(func,N,omega,b1,Lmax=1,errF=0.1,exparams=()):
+        by John Swoboda
+        This function will numerically integrate a Sommerfeld like integral, int(exp(-jwk)f(k),k=0..inf)
+        using the ERF transform and 2N+1 samples and at most Lmax loops. If the normalized difference
+        between the previous estimate of the output Xk is less then the parameter errF then the loop stops
+        and a flag that represents convergence is set to true. A number of loops is also output as well.
+        This function uses sommerfelderf to do the integration
+        Inputs
+        func: A function that is used to create f(k).
+        N: The integration uses 2N+1 samples to do the integration.
+        omega: The Frequency array in radians per second.
+        b1: The inital bounds of the first try and then step size for each subsiquent
+        integral.
+        Lmax: default 1:The maximum number of repeats of the integration before the loop finishes.
+        errF: default .1: The threshold of the normalized difference between the new
+        iteration and the old to stop to stop the iteration.
+        exparams: default (): Any extra params other then k to create f(k).
+        output
+        Xk - The integrated data that is the same length as omega.
+        flag_c - A convergence flag.
+        irep - The number of repitiions until convergence. """
+    Xk =np.zeros_like(omega)*1j
+    for irep in range(Lmax):
+
+        Xktemp = sommerfelderf(func,N,omega,b1*irep,b1*(irep+1),exparams)
+        Xkdiff = np.sqrt(np.sum(np.power(np.abs(Xktemp),2.0)))
+        Xkpow = np.sqrt(np.sum(np.power(np.abs(Xk+Xktemp))))
+        Xk = Xk+Xktemp
+        if Xkdiff/Xkpow<errF:
+            flag_c = True
+            break
+    return (Xk,flag_c,irep+1)
+def sommerfelderf(func,N,omega,a,b,exparams=()):
+    """ sommerfelderf(func,N,omega,a,b,exparams=())
+        by John Swoboda
+        This function will numerically integrate a Sommerfeld like integral, int(exp(-jwk)f(k),k=a..b)
+        using the ERF transform and 2N+1 samples. This technique is from the paper B. L. Ooi 2007.
+        Inputs
+        func: A function that is used to create f(k).
+        N: The integration uses 2N+1 samples to do the integration.
+        omega: The Frequency array in radians per second.
+        a: Lower bound of the integral.
+        b: Upper bound of teh integral.
+        exparams: default (): Any extra params other then k to create f(k).
+        output
+        Xk - The integrated data that is the same length as omega."""
 
     nvec = np.arange(-N,N+1)
 
@@ -104,14 +187,11 @@ def sommerfelderf(func,N,theta,a,b,exparams=None):
 
     An = np.cosh(nvec*h)*np.exp(-np.power(np.sinh(nvec*h),2))
 
-    Xk3 = np.zeros_like(theta)*1.0j
+    Xk3 = np.zeros_like(omega)*1.0j
 
-    if exparams is None:
-        fk = func(kn)
-    else:
-        fk = func(kn,*exparams)
-        #XXX make matrix version
-    for omegk, omeg in enumerate(theta):
-        fkn = np.exp(-1j*kn*omeg) *fk
-        Xk3[omegk] = np.sum(fkn*An)
+    fk = func(kn,*exparams)
+    kmat = np.tile(kn[:,np.newaxis],(1,len(omega)))
+    omegamat = np.tile(omega[np.newaxis,:],(len(kn),1))
+    Xk3 = np.dot(np.exp(-1j*kmat*omegamat),An*fk);
+
     return Xk3*h*(b-a)/np.sqrt(np.pi)
