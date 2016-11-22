f6e216a1d02dd3637003b1efb62d80822c007a98
scienceopen <scienceopen@users.noreply.github.com>
2016-11-21 19:10:41 -0500

    inwork

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import subprocess
+from setuptools import setup
+
+try:
+    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
+except Exception as e:
+    pass
+
+
+config = {
+    'description': 'ISR constants',
+    'author': 'John Swoboda',
+    'url': 'https://github.com/jswoboda/PythonISRUtilities.git',
+    'version': '1',
+    'packages': ['isrutilities'],
+    'name':      'isrutilities'
+}
+
+setup(**config)
